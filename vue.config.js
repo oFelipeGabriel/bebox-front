ada9350e371e615b147913c4033493793787ad53
oFelipeGabriel <felipegabriel@rocketmail.com>
2019-10-31 11:14:37 -0300

    fix vus.config

--- vue.config.js
+++ vue.config.js
@@ -12,15 +12,15 @@ module.exports = {
     }
   }
 };
-
-module.exports = {
-  // ...
-  resolve: {
-    alias: {
-      // If using the runtime only build
-      vue$: 'vue/dist/vue.runtime.esm.js' // 'vue/dist/vue.runtime.common.js' for webpack 1
-      // Or if using full build of Vue (runtime + compiler)
-      // vue$: 'vue/dist/vue.esm.js'      // 'vue/dist/vue.common.js' for webpack 1
-    }
-  }
-}
+// 
+// module.exports = {
+//   // ...
+//   resolve: {
+//     alias: {
+//       // If using the runtime only build
+//       vue$: 'vue/dist/vue.runtime.esm.js' // 'vue/dist/vue.runtime.common.js' for webpack 1
+//       // Or if using full build of Vue (runtime + compiler)
+//       // vue$: 'vue/dist/vue.esm.js'      // 'vue/dist/vue.common.js' for webpack 1
+//     }
+//   }
+// }
