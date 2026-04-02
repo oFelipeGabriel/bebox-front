86df3705c7dc0ad43526b3ae95812d21b56e4dca
Felipe Gabriel <felipegbfaria@gmail.com>
2026-04-02 15:06:51 -0300

    Updates

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module'
+  },
+  rules: {}
+}
